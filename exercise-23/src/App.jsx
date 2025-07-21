1bac0458fa8b78fb8f8ad10702ac1896fa9792d0
guutaale <mxasan16@gmail.com>
2025-07-21 16:36:41 +0300

    Ex#23 completed

---
+++ exercise-23/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import FormValidation from "./FormValidation";
+const App = () => {
+  return (
+    <div className="min-h-screen bg-zinc-50 py-5 px-4 sm:px-6 lg:px-8">
+      <FormValidation />
+    </div>
+  );
+};
+
+export default App;
